d9706e7137c06df1ba4965e48be236bae6b29af1
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2025-12-03 14:39:50 +0530

    added backend render link to api.js

--- todo-frontend/src/api/api.js
+++ todo-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://todo-backend-188g.onrender.com/api",
 });
 
 export const getAllTasks = () => api.get("/todos");
