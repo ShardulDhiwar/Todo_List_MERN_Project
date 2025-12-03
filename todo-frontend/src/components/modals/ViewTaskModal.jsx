cb7a61ed0cf5ce701e8549ba64dedc4d66b51b7e
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2025-12-03 11:29:01 +0530

    Final Commit Before Mentor Review

--- todo-frontend/src/components/modals/ViewTaskModal.jsx
+++ todo-frontend/src/components/modals/ViewTaskModal.jsx
@@ -3,9 +3,10 @@
 const EditTaskModal = ({ task, onClose }) => {
   return (
     <div className="fixed inset-0 bg-black/40 flex justify-center items-center">
-      <div className="bg-white p-6 rounded-lg w-full max-w-md shadow-lg">
+      <div className="bg-white p-4 md:p-6 rounded-lg w-11/12 max-w-xs sm:max-w-sm md:max-w-md shadow-lg">
         <h2 className="text-xl font-bold mb-4 text-center">Task Details</h2>
 
+        {/* Form taken from Tailwind CSS docs */}
         <input
           type="text"
           value={task.title}
