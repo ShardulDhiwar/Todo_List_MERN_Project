ee413a4e3969012844ff6046ad28debce6e652c4
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2025-12-03 18:12:44 +0530

    final final commit

--- todo-frontend/src/components/cards/StatsCard.jsx
+++ todo-frontend/src/components/cards/StatsCard.jsx
@@ -3,13 +3,6 @@ import React from "react";
 const StatsCard = ({ title, count, color, icon }) => {
   return (
     <>
-      {/* <div className={`p-6 h-50 rounded-xl shadow-md ${color}`}>
-        <div className="flex item-center gap-3">
-          <div className="text-2xl">{icon}</div>
-          <h1 className="text-lg font-semibold mt-3">{title}</h1>
-        </div>
-        <p className="text-4xl font-bold mt-3 ">{count}</p>
-      </div> */}
       <div className={`p-4 md:p-6 rounded-xl shadow-md ${color}`}>
         <div className="flex items-center gap-3">
           <div className="text-xl md:text-2xl">{icon}</div>
--- todo-frontend/src/components/navbar/Navbar.jsx
+++ todo-frontend/src/components/navbar/Navbar.jsx
@@ -1,13 +1,5 @@
 const Navbar = () => {
   return (
-    // <nav className="bg-blue-50 p-4 font-semibold text-3xl shadow text-center">
-    //   <div className="flex items-center gap-2">
-    //     <div className="w-7 h-7 rounded">
-    //       <img src="/to-do-list.gif" alt="list" />
-    //     </div>
-    //     <h1 className="text-xl font-semibold">MyTodoList</h1>
-    //   </div>
-    // </nav>
     <nav className="bg-blue-50 p-4 shadow ">
       <div className="flex items-center gap-3 justify-center">
         <img
